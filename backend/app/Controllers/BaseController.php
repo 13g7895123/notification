6b446f82d79ca7441b0402da80b86bd89644d888
jarvis <13g7895123@gmail.com>
2025-12-30 13:58:55 +0800

    feat: 新增生成 UUID v4 的方法

--- backend/app/Controllers/BaseController.php
+++ backend/app/Controllers/BaseController.php
@@ -73,6 +73,18 @@ class BaseController extends ResourceController
         );
     }
 
+    /**
+     * 生成 UUID v4
+     */
+    protected function generateUuid(): string
+    {
+        $data = random_bytes(16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // Version 4
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // Variant RFC 4122
+
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
+
 
 
     /**
